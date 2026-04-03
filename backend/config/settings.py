6834eb5ba934f8d5937f67d256e253a66ebc7920
bhushantile20 <bhushantile2003@gmail.com>
2026-04-03 12:07:20 +0530

    Setup: Azure production configuration and github actions

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -27,13 +27,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p-#=c0c0-k3gb6$hg)a+jd&m3ydj8y89i@q@26hclc_nfez5-+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS', '').split(',')] if os.getenv('ALLOWED_HOSTS') else []
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
+if os.getenv('CORS_ALLOWED_ORIGINS'):
+    CORS_ALLOWED_ORIGINS += [origin.strip() for origin in os.getenv('CORS_ALLOWED_ORIGINS').split(',')]
 
 CORS_ALLOW_HEADERS = [
     "accept",
@@ -71,6 +73,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -167,3 +170,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
